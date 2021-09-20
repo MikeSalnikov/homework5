6e33e3ee60700cda544f6c3647633212c3419d94
MikeSalnikov <skynet99@mail.ru>
2021-09-20 14:16:44 +0300

    2nd commit

---
+++ hw5_1.py
@@ -0,0 +1,14 @@
+# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+# Об окончании ввода данных свидетельствует пустая строка.
+
+
+with open('file_to_1.txt', 'w+') as my_f:
+    text = input('Введите текст: ')
+    while text:
+        my_f.writelines(f"{text}\n")
+        text = input('Введите текст: ')
+        if not text:
+            break
+    my_f.seek(0)
+    content = my_f.read()
+    print(content)
